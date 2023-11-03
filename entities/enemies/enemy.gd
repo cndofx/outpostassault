5df31e8532371b2fb6e5b98e4551e347b2c22d9a
cndofx <19335833+cndofx@users.noreply.github.com>
2023-11-02 23:29:59 -0600

    lesson 5

---
+++ entities/enemies/enemy.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed: int = 150
+@export var objective: Node2D
+
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+
+func _ready() -> void:
+	nav_agent.max_speed = speed
+	nav_agent.target_position = objective.global_position
+	
+func _physics_process(delta: float) -> void:
+	var next_position: Vector2 = nav_agent.get_next_path_position()
+	var current_position: Vector2 = self.global_position
+	var new_velocity: Vector2 = current_position.direction_to(next_position) * speed
+	self.velocity = new_velocity
+	self.move_and_slide()
