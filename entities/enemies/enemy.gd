e3af9107a411bde3074040ac252976873569c8ef
cndofx <19335833+cndofx@users.noreply.github.com>
2023-11-03 01:04:32 -0600

    lesson 7

--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
+class_name Enemy
 
 @export var speed: int = 150
-@export var objective: Node2D
+#@export var objective: Node2D
 
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 
 func _ready() -> void:
 	nav_agent.max_speed = speed
-	nav_agent.target_position = objective.global_position
+#	nav_agent.target_position = objective.global_position
 	
 func _physics_process(delta: float) -> void:
 	var next_position: Vector2 = nav_agent.get_next_path_position()
@@ -15,3 +16,6 @@ func _physics_process(delta: float) -> void:
 	var new_velocity: Vector2 = current_position.direction_to(next_position) * speed
 	self.velocity = new_velocity
 	self.move_and_slide()
+
+func set_target(target: Vector2) -> void:
+	nav_agent.target_position = target
---
+++ maps/spawner/spawner.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+@export var objective: Node2D
+
+@export_range(0.5, 5.0, 0.5) var spawn_rate: float = 2.0
+@export var wave_count: int = 3
+@export var enemies_per_wave: int = 10
+
+var spawn_locations: Array[Node2D] = []
+var current_wave: int = 0
+var current_enemy_count: int = 0
+
+@onready var spawn_container: Node2D = $SpawnContainer
+@onready var wave_timer: Timer = $WaveTimer
+@onready var spawn_timer: Timer = $SpawnTimer
+
+var infantry_t1: PackedScene = preload("res://entities/enemies/infantry/infantry_t1.tscn")
+
+func _ready() -> void:
+	for child in spawn_container.get_children():
+		spawn_locations.append(child)
+	wave_timer.start()
+
+
+func _start_wave():
+	current_wave += 1
+	spawn_timer.start()
+	current_enemy_count = 0
+
+func _end_wave():
+	if current_wave < wave_count:
+		wave_timer.start()
+
+func _spawn_enemy(scene: PackedScene):
+	var enemy: Enemy = scene.instantiate()
+	get_parent().add_child(enemy)
+	enemy.set_target(objective.global_position)
+	enemy.global_position = spawn_locations.pick_random().global_position
+	current_enemy_count += 1
+
+func _on_wave_timer_timeout() -> void:
+	_start_wave()
+
+
+func _on_spawn_timer_timeout() -> void:
+	if current_enemy_count < enemies_per_wave:
+		_spawn_enemy(infantry_t1)
+		var spawn_delay: float = randf_range(spawn_rate / 2.0, spawn_rate)
+		spawn_timer.start(spawn_delay)
+	else:
+		_end_wave()
